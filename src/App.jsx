dce83621a101e930ab4ab3bd3a5dbefcf4c8cd76
krestwastaken <martynskelvin@gmail.com>
2025-12-07 20:20:50 +0100

    state commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 const App = () => {
+  const [isSidebarOpen, setIsSidebarOpen]=useState(false)
+
+  const openSidebar = ()=>{
+    setIsSidebarOpen(true)
+  }
+  const closeSidebar = ()=>{
+    setIsSidebarOpen(false)
+  }
   return <h2>Strapi Starter</h2>;
 };
 export default App;
