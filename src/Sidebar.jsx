5fcafa499cb4de2548d61a02cda44f3a994d5ce0
krestwastaken <martynskelvin@gmail.com>
2025-12-09 11:15:57 +0100

    sidebar component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return <aside className="sidebar">
+    <div className="sidebar-container">
+        <button className="close-btn">
+            <FaTimes/>
+        </button>
+        <div className="sidebar-links">
+            {sublinks.map((item)=>{
+                return <p>page</p>
+            })}
+        </div>
+    </div>
+  </aside>;
+};
 
-export default Sidebar
+export default Sidebar;
